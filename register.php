a97758f9293504df9d7c4cad4fd2184fcf89a254
hace4 <42021756+hace4@users.noreply.github.com>
2023-03-28 13:13:32 +0300

    ZXXXXXXX

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 'Off');
+error_reporting(E_ALL) 
+
+?>
 <?php
 require_once "verstka/register.html";
 
@@ -6,6 +11,18 @@ $db = new SQLite3("MARKET_db.db");
 $login = $_POST["login"];
 $password = $_POST["password"];
 $name = $_POST["name"];
-$db->query("INSERT INTO `users` (`login`, `password`, `F.I.O`) VALUES('$login', '$password', '$name')")
+if(strlen($password) <6){
+    echo '<div class=regist1><b><p>минимальная длина пароля 6</p></b></div>';
+    exit();
+} else if(strlen($login) < 5){
+    echo '<div class=regist1><b><p>минимальная длина логина 2</p></b></div>';
+}
+else{
+    $db->query("INSERT INTO `users` (`login`, `password`, `F.I.O`) VALUES('$login', '$password', '$name')");
+    $db->close();
+    header('Location: /');
+    exit();
+}
 
-?>  
+
+  
