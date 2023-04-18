8dba5e347ef44e02c713c2fd0e3a04d69c93c0ab
hace4 <42021756+hace4@users.noreply.github.com>
2023-04-19 00:26:02 +0300

    mske_base oop

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'>
     <link rel="stylesheet" href="verstka\static\main.css">
-    <title>Document</title>
+    <link rel="shortcut icon" href="favonic.ico" type="image/x-icon">
+    <title>SHMSHOP</title>
 </head>
 <body>
     <div class="bg"></div>
--- modules/aut._checker.php
+++ modules/aut._checker.php
@@ -5,33 +5,72 @@ $answer = new database();
 $login = $_POST["login"];
 $password = $_POST["password"];
 if ($login && $password) {
-    if ($login && $password != null) {
-        $result = $answer->get_logi_pass($login);
 
-        if ($result[0] == $login) {
+    $result = $answer->get_logi_pass($login);
 
-            if ($result[1] == $password) {
-                $_SESSION['login'] = $login;
+    if ($result[0] == $login) {
 
-                if ($login == 'root') {
-                    header('Location: ../root.php');
-                } else {
-                    header('Location: /');}
+        if ($result[1] == $password) {
+            $_SESSION['login'] = $login;
 
+            if ($login == 'root') {
+                header('Location: ../root.php');
             } else {
-                $_SESSION['message'] = 'Логин или пароль неверны';
-                header('Location: ../verstka/aut.php');}
+                header('Location: /');}
 
         } else {
             $_SESSION['message'] = 'Логин или пароль неверны';
             header('Location: ../verstka/aut.php');}
 
     } else {
-        $_SESSION['message'] = "Заполните все поля";
+        $_SESSION['message'] = 'Логин или пароль неверны';
         header('Location: ../verstka/aut.php');}
+
 } else {
     $_SESSION['message'] = "Заполните все поля";
     header('Location: ../verstka/aut.php');
     header("HTTP/1.0 302 Moved Temporarily", true, 302);
     header("Location: " . $_SERVER['REQUEST_URI'], true);
     exit();}
+class aut_checker
+{
+    private $login;
+    private $password;
+    private $answer;
+    private $result;
+    public function __construct()
+    {
+        require_once '../modules/db.php';
+        $this->answer = new database();
+        $this->login = $_POST["login"];
+        $this->password = $_POST["password"];
+    }
+    private function Availability_check()
+    {
+        if (!empty($this->login) && !empty($this->password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private function correctness_login_check()
+    {
+        if ($this->Availability_check()) {
+            $this->result = $this->answer->get_logi_pass($this->login);
+            if ($this->result[0] == $this->login) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    private function correctness_password_check()
+    {
+        if ($this->result[1] == $this->password) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+?>
