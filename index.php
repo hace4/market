20db13b09fb00cad1eb2ac5f8167cddb0e81810f
hace4 <42021756+hace4@users.noreply.github.com>
2023-04-20 02:55:04 +0300

    formator

--- index.php
+++ index.php
@@ -3,36 +3,38 @@ session_start()
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'>
+    <link rel="stylesheet" href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'>
     <link rel="stylesheet" href="verstka\static\main.css">
     <link rel="shortcut icon" href="img/favonic.ico" type="image/x-icon">
     <title>SHMSHOP</title>
 </head>
+
 <body>
     <div class="bg"></div>
     <div class="bg bg2"></div>
     <div class="bg bg3"></div>
     <main>
         <header>
-        <div class="top-info">
-            <H1 class="logo">Magazin</H1>
-            <?php
-                if($_SESSION['login'] == 'root'){
+            <div class="top-info">
+                <H1 class="logo">Magazin</H1>
+                <?php
+                if ($_SESSION['login'] == 'root') {
                     echo "<a href='root.php'> админка <a/>";
                 }
-            ?>
-            <nav class='reg'>
-            <a href="">В корзине</a>
+                ?>
+                <nav class='reg'>
+                    <a href="">В корзине</a>
                     <a href="verstka\aut.php">signup</a>
                     <a href="verstka\register.php">signin</a>
-            </nav>
+                </nav>
 
 
-        </div>
+            </div>
         </header>
         <h2 class=typing>Главная страница</h2>
         <div class="conten1">
@@ -41,7 +43,8 @@ session_start()
             $show = new show_products();
             $show->show();
             ?>
-         </div>
+        </div>
     </main>
 </body>
+
 </html>
