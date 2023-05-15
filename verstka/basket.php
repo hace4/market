7494302429491794904fe0e1706a6b4cabdf2c04
hace4 <42021756+hace4@users.noreply.github.com>
2023-05-15 15:52:37 +0300

    favonic

--- verstka/basket.php
+++ verstka/basket.php
@@ -11,7 +11,7 @@ require_once '../config.php';
 <meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
 <link rel = 'stylesheet' href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'>
 <link rel = 'stylesheet' href = 'static\main.css'>
-<link rel = 'shortcut icon' href = 'img/favonic.ico' type = 'image/x-icon'>
+<link rel="shortcut icon" href="../../faviconico" type="image/x-icon">
 
 <title>корзина</title>
 </head>
--- verstka/register.php
+++ verstka/register.php
@@ -11,7 +11,7 @@ require_once '../config.php';
 <meta http-equiv = 'X-UA-Compatible' content = 'IE=edge'>
 <meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
 <link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
-<link rel = 'shortcut icon' href = 'img/favonic.ico' type = 'image/x-icon'>
+<link rel="shortcut icon" href="../../faviconico" type="image/x-icon">
 <link rel = 'stylesheet' href = 'static/style.css'>
 <script src = 'https://unpkg.com/swup@3'></script>
 <script>const swup = new Swup();
